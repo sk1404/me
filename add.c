dcffcf9c501da6947afaac29d3472621fcc20557
sk1404 <sheshadri1404@gmail.com>
2025-07-18 18:39:51 +0530

    1add.c

---
+++ add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s num1 num2\n", argv[0]);
+        return 1;
+    }
+
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    printf("Sum: %d\n", a + b);
+    return 0;
+}
