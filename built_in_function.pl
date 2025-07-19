c5c2bc60e89cb7092e920ad437b3ef931fd29ad4
sk1404 <sheshadri1404@gmail.com>
2025-07-19 08:08:52 +0530

    Update built_in_function.pl

--- built_in_function.pl
+++ built_in_function.pl
@@ -2,31 +2,43 @@
 use strict;
 use warnings;
 
-# Scalar variable (single value)
-my $name = "Ravi";               # Functions: length, uc, lc
-print "Name: $name\n";
-print "Name in uppercase: ", uc($name), "\n";
+# Scalar variable example (single value)
+my $name = "Sheshadri";
+print "Hello, $name\n";
 
-# Numeric scalar
-my $age = 21;                    # Functions: int, sqrt
-print "Age: $age, Square root: ", sqrt($age), "\n";
+my $marks = 88;
+print "Marks: $marks\n";
 
-# Array variable (list of values)
-my @colors = ("red", "blue");    # Functions: push, pop, sort
-push(@colors, "green");
+# Array variable example (list of values)
+my @colors = ("red", "green", "blue");
 print "Colors: @colors\n";
-print "Sorted: ", join(", ", sort @colors), "\n";
+print "First color: $colors[0]\n";
 
-# Hash variable (key-value pairs)
-my %student = ("name" => "Anu", "usn" => "1BM24CS001"); # Functions: keys, values, exists
-print "Student name: $student{name}\n";
+# Hash variable example (key-value pairs)
+my %student = (
+    name => "Anu",
+    usn  => "1BM24CS001"
+);
+print "Student Name: $student{name}\n";
+print "USN: $student{usn}\n";
 
-# User input and chomp()
-print "Enter your city: ";
-chomp(my $city = <STDIN>);       # chomp removes newline
-print "City entered: $city\n";
+# Use scalar to count array elements
+my $count = scalar @colors;
+print "Total colors: $count\n";
 
-# Loop through array
-foreach my $color (@colors) {
-    print "Color: $color\n";
+# Convert array to uppercase using map
+my @upper = map { uc($_) } @colors;
+print "Uppercase Colors: @upper\n";
+
+# Loop through hash keys
+foreach my $key (keys %student) {
+    print "$key => $student{$key}\n";
 }
+
+# User input with chomp
+print "Enter a value: ";
+chomp(my $input = <STDIN>);
+print "You entered: $input\n";
+
+# Use length() on a scalar
+print "Length of name: ", length($name), "\n";
