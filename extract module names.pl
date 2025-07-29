50cf417816513a443fede0e37c7c9f1f0fa6a825
sk1404 <sheshadri1404@gmail.com>
2025-07-29 21:28:57 +0530

    extract module names.pl

--- extract module names.pl
+++ extract module names.pl
@@ -1,29 +1,41 @@
-open(F, "design.v") or die "Can't open file";
-open(O, ">output.csv");
+#!/usr/bin/perl
+use strict;
+use warnings;
 
-print O "Module,Direction,Port\n";
+open(my $in, "<", "design.v") or die "Can't open file: $!";
+open(my $out, ">", "output.csv") or die "Can't write to output.csv";
 
-while(<F>) {
+print $out "Module,Direction,Port\n";
+
+my $module;
+
+while (<$in>) {
     if (/module\s+(\w+)/) {
         $module = $1;
     }
+
     if (/input\s+(.*);/) {
-        @ports = split(",", $1);
-        foreach $p (@ports) {
-            $p =~ s/[\[\]\d:\s]//g;  # clean brackets and spaces
-            print O "$module,input,$p\n";
+        my $line = $1;
+        $line =~ s/\[[^\]]+\]//g;  # remove bit-widths
+        my @ports = split(/\s*,\s*/, $line);
+        foreach my $p (@ports) {
+            $p =~ s/^\s+|\s+$//g;
+            print $out "$module,input,$p\n" if $p;
         }
     }
+
     if (/output\s+(.*);/) {
-        @ports = split(",", $1);
-        foreach $p (@ports) {
-            $p =~ s/[\[\]\d:\s]//g;
-            print O "$module,output,$p\n";
+        my $line = $1;
+        $line =~ s/\[[^\]]+\]//g;
+        my @ports = split(/\s*,\s*/, $line);
+        foreach my $p (@ports) {
+            $p =~ s/^\s+|\s+$//g;
+            print $out "$module,output,$p\n" if $p;
         }
     }
 }
 
-close(F);
-close(O);
+close($in);
+close($out);
 
 print "Done. See output.csv\n";
