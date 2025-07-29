b6146e3b276aa1b32f4605db7f2a3e5863ac676b
sk1404 <sheshadri1404@gmail.com>
2025-07-29 21:43:15 +0530

    verilog_file_count.pl

--- verilog_file_count.pl
+++ verilog_file_count.pl
@@ -2,24 +2,60 @@
 use strict;
 use warnings;
 
-# Input Verilog file (change as needed)
-my $verilog_file = 'design.v';
+# Check for input file
+die "Usage: perl count_verilog_elements.pl <file.v>\n" unless @ARGV == 1;
+my $file = $ARGV[0];
 
 # Counters
-my ($module_count, $always_count, $flipflop_count) = (0, 0, 0);
+my ($module_count, $always_count, $ff_count) = (0, 0, 0);
 
-# Open the file
-open(my $fh, '<', $verilog_file) or die "Cannot open $verilog_file: $!";
+# Flip-flop module names to match (extend if needed)
+my @ff_names = qw(DFF FDRE DFFR DFFS DFFSR);
+my $ff_regex = join("|", @ff_names);
 
-while (my $line = <$fh>) {
-    $module_count++    if $line =~ /^\s*module\b/;
-    $always_count++    if $line =~ /^\s*always\b/;
-    $flipflop_count++  if $line =~ /\b(DFF|FDRE)\b/;
+# Open Verilog file
+open my $fh, '<', $file or die "Cannot open $file: $!";
+
+while (<$fh>) {
+    # Remove comments
+    s|//.*||;
+
+    # Count module declarations
+    $module_count++ if /\bmodule\b/;
+
+    # Count always blocks
+    $always_count++ if /\balways\b/;
+
+    # Count flip-flop instantiations
+    $ff_count++ if /\b($ff_regex)\b/;
 }
 
 close $fh;
 
-# Display results
-print "Module Declarations : $module_count\n";
-print "Always Blocks       : $always_count\n";
-print "Flip-Flop Instances : $flipflop_count\n";
+# Print results
+print "Module declarations : $module_count\n";
+print "Always blocks       : $always_count\n";
+print "Flip-flop instances : $ff_count\n";
+
+
+
+
+
+
+module top;
+  reg clk, rst, d;
+  wire q;
+
+  DFF dff1 (.clk(clk), .d(d), .q(q));
+  FDRE fdre1 (.C(clk), .D(d), .Q(q), .R(rst));
+
+  always @(posedge clk) begin
+    // some logic
+  end
+endmodule
+
+
+
+Module declarations : 1
+Always blocks       : 1
+Flip-flop instances : 2
